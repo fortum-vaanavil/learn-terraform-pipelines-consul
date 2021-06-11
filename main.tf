87565239628349cb93a09163410152540867cd4f
Ville Väänänen <ville.vaananen@fortum.com>
2021-06-11 12:12:41 +0300

    Configure workspace

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ terraform {
     organization = "infrastructure-pipelines-workshop"
 
     workspaces {
-      name = "john-d-consul"
+      name = "ville-v-consul"
     }
   }
   required_providers {
